c10c899ff6286cf69176ee4681a646fd5b25d787
STC <STC@DESKTOP-4AAGR7G>
2024-04-23 13:46:28 +0800

    Add project files.

---
+++ Resco_Calculator/Form1.Designer.vb
@@ -0,0 +1,293 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btn1 = New Button()
+        btn2 = New Button()
+        btn3 = New Button()
+        btndivide = New Button()
+        btn5 = New Button()
+        bnt6 = New Button()
+        btn7 = New Button()
+        bntmultiple = New Button()
+        bnt9 = New Button()
+        btn10 = New Button()
+        btn11 = New Button()
+        btnminus = New Button()
+        btn13 = New Button()
+        btnadd = New Button()
+        btn15 = New Button()
+        btn16 = New Button()
+        bntequal = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        TxtBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btn1
+        ' 
+        btn1.Font = New Font("Showcard Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        btn1.Location = New Point(45, 71)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(57, 38)
+        btn1.TabIndex = 1
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = True
+        ' 
+        ' btn2
+        ' 
+        btn2.Font = New Font("Showcard Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        btn2.Location = New Point(108, 71)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(57, 38)
+        btn2.TabIndex = 2
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = True
+        ' 
+        ' btn3
+        ' 
+        btn3.Font = New Font("Showcard Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        btn3.Location = New Point(172, 71)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(57, 36)
+        btn3.TabIndex = 3
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = True
+        ' 
+        ' btndivide
+        ' 
+        btndivide.Font = New Font("Showcard Gothic", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        btndivide.Location = New Point(251, 73)
+        btndivide.Name = "btndivide"
+        btndivide.Size = New Size(63, 35)
+        btndivide.TabIndex = 4
+        btndivide.Text = "/"
+        btndivide.UseVisualStyleBackColor = True
+        ' 
+        ' btn5
+        ' 
+        btn5.Font = New Font("Showcard Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        btn5.Location = New Point(45, 115)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(58, 38)
+        btn5.TabIndex = 5
+        btn5.Text = "4"
+        btn5.UseVisualStyleBackColor = True
+        ' 
+        ' bnt6
+        ' 
+        bnt6.Font = New Font("Showcard Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        bnt6.Location = New Point(108, 116)
+        bnt6.Name = "bnt6"
+        bnt6.Size = New Size(58, 37)
+        bnt6.TabIndex = 6
+        bnt6.Text = "5"
+        bnt6.UseVisualStyleBackColor = True
+        ' 
+        ' btn7
+        ' 
+        btn7.Font = New Font("Showcard Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        btn7.Location = New Point(172, 116)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(59, 37)
+        btn7.TabIndex = 7
+        btn7.Text = "6"
+        btn7.UseVisualStyleBackColor = True
+        ' 
+        ' bntmultiple
+        ' 
+        bntmultiple.Font = New Font("Showcard Gothic", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        bntmultiple.Location = New Point(252, 115)
+        bntmultiple.Name = "bntmultiple"
+        bntmultiple.Size = New Size(63, 38)
+        bntmultiple.TabIndex = 5
+        bntmultiple.Text = "*"
+        bntmultiple.UseVisualStyleBackColor = True
+        ' 
+        ' bnt9
+        ' 
+        bnt9.Font = New Font("Showcard Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        bnt9.Location = New Point(45, 159)
+        bnt9.Name = "bnt9"
+        bnt9.Size = New Size(58, 38)
+        bnt9.TabIndex = 5
+        bnt9.Text = "7"
+        bnt9.UseVisualStyleBackColor = True
+        ' 
+        ' btn10
+        ' 
+        btn10.Font = New Font("Showcard Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        btn10.Location = New Point(109, 159)
+        btn10.Name = "btn10"
+        btn10.Size = New Size(58, 37)
+        btn10.TabIndex = 6
+        btn10.Text = "8"
+        btn10.UseVisualStyleBackColor = True
+        ' 
+        ' btn11
+        ' 
+        btn11.Font = New Font("Showcard Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        btn11.Location = New Point(173, 159)
+        btn11.Name = "btn11"
+        btn11.Size = New Size(58, 38)
+        btn11.TabIndex = 5
+        btn11.Text = "9"
+        btn11.UseVisualStyleBackColor = True
+        ' 
+        ' btnminus
+        ' 
+        btnminus.Font = New Font("Showcard Gothic", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        btnminus.Location = New Point(252, 159)
+        btnminus.Name = "btnminus"
+        btnminus.Size = New Size(63, 38)
+        btnminus.TabIndex = 5
+        btnminus.Text = "-"
+        btnminus.UseVisualStyleBackColor = True
+        ' 
+        ' btn13
+        ' 
+        btn13.Font = New Font("Showcard Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        btn13.Location = New Point(45, 203)
+        btn13.Name = "btn13"
+        btn13.Size = New Size(58, 38)
+        btn13.TabIndex = 5
+        btn13.Text = "C"
+        btn13.UseVisualStyleBackColor = True
+        ' 
+        ' btnadd
+        ' 
+        btnadd.Font = New Font("Showcard Gothic", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        btnadd.Location = New Point(252, 203)
+        btnadd.Name = "btnadd"
+        btnadd.Size = New Size(63, 38)
+        btnadd.TabIndex = 5
+        btnadd.Text = "+"
+        btnadd.UseVisualStyleBackColor = True
+        ' 
+        ' btn15
+        ' 
+        btn15.Font = New Font("Showcard Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        btn15.Location = New Point(109, 202)
+        btn15.Name = "btn15"
+        btn15.Size = New Size(58, 39)
+        btn15.TabIndex = 6
+        btn15.Text = "0"
+        btn15.UseVisualStyleBackColor = True
+        ' 
+        ' btn16
+        ' 
+        btn16.Font = New Font("Showcard Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        btn16.Location = New Point(173, 203)
+        btn16.Name = "btn16"
+        btn16.Size = New Size(58, 38)
+        btn16.TabIndex = 5
+        btn16.Text = "."
+        btn16.UseVisualStyleBackColor = True
+        ' 
+        ' bntequal
+        ' 
+        bntequal.Font = New Font("Showcard Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        bntequal.Location = New Point(44, 247)
+        bntequal.Name = "bntequal"
+        bntequal.Size = New Size(270, 39)
+        bntequal.TabIndex = 8
+        bntequal.Text = "="
+        bntequal.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(321, 216)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(41, 15)
+        Label1.TabIndex = 9
+        Label1.Text = "Label1"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(320, 261)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(41, 15)
+        Label2.TabIndex = 10
+        Label2.Text = "Label2"
+        ' 
+        ' TxtBox1
+        ' 
+        TxtBox1.Location = New Point(45, 31)
+        TxtBox1.Name = "TxtBox1"
+        TxtBox1.Size = New Size(263, 23)
+        TxtBox1.TabIndex = 11
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(398, 296)
+        Controls.Add(TxtBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(bntequal)
+        Controls.Add(btn7)
+        Controls.Add(btn15)
+        Controls.Add(btn10)
+        Controls.Add(bnt6)
+        Controls.Add(bntmultiple)
+        Controls.Add(btn16)
+        Controls.Add(btn11)
+        Controls.Add(btnadd)
+        Controls.Add(btn13)
+        Controls.Add(btnminus)
+        Controls.Add(bnt9)
+        Controls.Add(btn5)
+        Controls.Add(btndivide)
+        Controls.Add(btn3)
+        Controls.Add(btn2)
+        Controls.Add(btn1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btndivide As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents bnt6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents bntmultiple As Button
+    Friend WithEvents bnt9 As Button
+    Friend WithEvents btn10 As Button
+    Friend WithEvents btn11 As Button
+    Friend WithEvents btnminus As Button
+    Friend WithEvents btn13 As Button
+    Friend WithEvents btnadd As Button
+    Friend WithEvents btn15 As Button
+    Friend WithEvents btn16 As Button
+    Friend WithEvents bntequal As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents RTxtBox1 As RichTextBox
+    Friend WithEvents TxtBox1 As TextBox
+End Class
---
+++ Resco_Calculator/Form1.vb
@@ -0,0 +1,96 @@
+﻿Public Class Form1
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        TxtBox1.Text = TxtBox1.Text + "1"
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        TxtBox1.Text = TxtBox1.Text + "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        TxtBox1.Text = TxtBox1.Text + "3"
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        TxtBox1.Text = TxtBox1.Text + "4"
+    End Sub
+
+    Private Sub btn13_Click(sender As Object, e As EventArgs) Handles btn13.Click
+        TxtBox1.Text = ""
+    End Sub
+
+    Private Sub bnt6_Click(sender As Object, e As EventArgs) Handles bnt6.Click
+        TxtBox1.Text = TxtBox1.Text + "5"
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        TxtBox1.Text = TxtBox1.Text + "6"
+    End Sub
+
+    Private Sub bnt9_Click(sender As Object, e As EventArgs) Handles bnt9.Click
+        TxtBox1.Text = TxtBox1.Text + "7"
+    End Sub
+
+    Private Sub btn10_Click(sender As Object, e As EventArgs) Handles btn10.Click
+        TxtBox1.Text = TxtBox1.Text + "8"
+    End Sub
+
+    Private Sub btn11_Click(sender As Object, e As EventArgs) Handles btn11.Click
+        TxtBox1.Text = TxtBox1.Text + "9"
+    End Sub
+
+    Private Sub btn15_Click(sender As Object, e As EventArgs) Handles btn15.Click
+        TxtBox1.Text = TxtBox1.Text + "0"
+    End Sub
+
+    Private Sub btn16_Click(sender As Object, e As EventArgs) Handles btn16.Click
+        TxtBox1.Text = TxtBox1.Text + "."
+    End Sub
+
+    Private Sub btndivide_Click(sender As Object, e As EventArgs) Handles btndivide.Click
+        Label1.Text = TxtBox1.Text
+        Label2.Text = "/"
+        TxtBox1.Text = ""
+    End Sub
+
+    Private Sub bntmultiple_Click(sender As Object, e As EventArgs) Handles bntmultiple.Click
+        Label1.Text = TxtBox1.Text
+        Label2.Text = "*"
+        TxtBox1.Text = ""
+    End Sub
+
+    Private Sub btnminus_Click(sender As Object, e As EventArgs) Handles btnminus.Click
+        Label1.Text = TxtBox1.Text
+        Label2.Text = "-"
+        TxtBox1.Text = ""
+    End Sub
+
+    Private Sub btnadd_Click(sender As Object, e As EventArgs) Handles btnadd.Click
+        Label1.Text = TxtBox1.Text
+        Label2.Text = "+"
+        TxtBox1.Text = ""
+    End Sub
+
+    Private Sub bntequal_Click(sender As Object, e As EventArgs) Handles bntequal.Click
+        If Label2.Text = "/" Then
+            TxtBox1.Text = Val(Label1.Text) / Val(TxtBox1.Text)
+            Label1.Text = ""
+            Label2.Text = "="
+        End If
+        If Label2.Text = "*" Then
+            TxtBox1.Text = Val(Label1.Text) * Val(TxtBox1.Text)
+            Label1.Text = ""
+            Label2.Text = "="
+        End If
+        If Label2.Text = "-" Then
+            TxtBox1.Text = Val(Label1.Text) - Val(TxtBox1.Text)
+            Label1.Text = ""
+            Label2.Text = "="
+        End If
+        If Label2.Text = "+" Then
+            TxtBox1.Text = Val(Label1.Text) + Val(TxtBox1.Text)
+            Label1.Text = ""
+            Label2.Text = "="
+        End If
+    End Sub
+End Class
